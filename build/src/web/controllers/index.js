c2d8236d5bcf41446e7d75072f445ab48233da4c
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-17 22:09:20 +0100

    running deployement on vercel

--- build/src/web/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../controllers/auth/auth.controller");
-require("../controllers/user/user.controller");
-require("../controllers/task/task.controller");
