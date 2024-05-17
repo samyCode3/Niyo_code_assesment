cb4504fb777a375b6d6331c9670e15a0352e5b07
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-17 23:01:30 +0100

    solving deployment issue

--- build/src/core/utils/tryCatch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncFunction = (func) => {
-    return (request, response, next) => {
-        func(request, response, next).catch((err) => next(err));
-    };
-};
-exports.default = asyncFunction;
