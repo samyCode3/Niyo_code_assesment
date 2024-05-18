14d50c99789cbd11fdec0c3ee6b051187ffb3f83
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-18 05:04:03 +0100

    running deployment again v11

--- src/core/utils/validation/env.validate.ts
+++ src/core/utils/validation/env.validate.ts
@@ -1,11 +1,23 @@
 
 
-// NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
-// PORT: Joi.number().default(3000),
-// DB_HOST: Joi.string().required(),
-// DB_PORT: Joi.number().default(5432),
-// DB_USER: Joi.string().required(),
-// DB_PASS: Joi.string().required(),
-// DB_NAME: Joi.string().required(),
-// REDIS_HOST: Joi.string().required(),
-// REDIS_PORT: Joi.number().default(6379),
+import joi, { ObjectSchema } from "joi";
+import validator from "../validator";
+
+
+export const validateTaskField = async (body: any) => {
+  const schema: ObjectSchema = joi.object().keys({
+    NODE_ENV: joi.string().valid('development', 'production', 'test').required(),
+    PORT: joi.number().default(5003),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().default(3306),
+    DB_USER: joi.string().required(),
+    DB_PASS: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    REDIS_URL: joi.string().required(),
+    SECRET_TOKEN: joi.string().required(),
+
+  }).unknown().required();
+
+  return validator(schema, body);
+};
+
