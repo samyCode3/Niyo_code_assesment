4fd1d725766e017c786a59921eb7f33f8d900f16
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-18 04:20:53 +0100

    running deployment again v56

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,13 +1,37 @@
 import "reflect-metadata";
-
-
-import { App } from "../web/application";
 import "../core/config/redis.config";
 import "../core/config/redis.config";
 
-export const bootstrap = async () => {
-  new App().setup();
-};
+import express, { NextFunction, Request, Response } from "express";
+import container from "../core/config/di-container";
+import { InversifyExpressServer } from "inversify-express-utils";
+import morgan from "morgan";
+import { globalErrorMiddleware } from "../core/middleware/global.error";
 
-bootstrap();
-  
+export class App {
+  setup() {
+    const server = new InversifyExpressServer(container, null, {
+      rootPath: "/api/v1",
+    });
+    
+    server.setConfig((app) => {
+      app.use(express.json());
+      app.get("/", (req: Request, res: Response, next: NextFunction) => {
+        return res.send(`Hello Niyo Api`);
+      });
+      app.use(morgan("dev"));
+    });
+
+    server.setErrorConfig((app) => {
+      app.all("*", (req: Request, res: Response) => {
+        return res.status(404).json({ error: 404, message: "Route not found" });
+      });
+      app.use(globalErrorMiddleware());
+    });
+
+    return server.build();
+  }
+}
+
+const appInstance = new App();
+const app = appInstance.setup();
