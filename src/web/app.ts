ab2fea64f28472a93606b691a2affb2be7485f5d
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-18 04:33:37 +0100

    running deployment again v57

--- src/web/app.ts
+++ src/web/app.ts
@@ -7,31 +7,29 @@ import container from "../core/config/di-container";
 import { InversifyExpressServer } from "inversify-express-utils";
 import morgan from "morgan";
 import { globalErrorMiddleware } from "../core/middleware/global.error";
-
-export class App {
-  setup() {
-    const server = new InversifyExpressServer(container, null, {
-      rootPath: "/api/v1",
+export const mySerever = () => {
+  const server = new InversifyExpressServer(container, null, {
+    rootPath: "/api/v1",
+  });
+  
+  server.setConfig((app) => {
+    app.use(express.json());
+    app.get("/", (req: Request, res: Response, next: NextFunction) => {
+      return res.send(`Hello Niyo Api`);
     });
-    
-    server.setConfig((app) => {
-      app.use(express.json());
-      app.get("/", (req: Request, res: Response, next: NextFunction) => {
-        return res.send(`Hello Niyo Api`);
-      });
-      app.use(morgan("dev"));
-    });
+    app.use(morgan("dev"));
+  });
 
-    server.setErrorConfig((app) => {
-      app.all("*", (req: Request, res: Response) => {
-        return res.status(404).json({ error: 404, message: "Route not found" });
-      });
-      app.use(globalErrorMiddleware());
+  server.setErrorConfig((app) => {
+    app.all("*", (req: Request, res: Response) => {
+      return res.status(404).json({ error: 404, message: "Route not found" });
     });
+    app.use(globalErrorMiddleware());
+  });
 
-    return server.build();
-  }
+  return server.build();
+
 }
 
-const appInstance = new App();
-const app = appInstance.setup();
+
+mySerever()
