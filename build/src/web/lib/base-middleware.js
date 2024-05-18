8c14b92796ecdf352312346bbe9dc7087677f9bb
samyCode3 <155715567+jarvisKing2@users.noreply.github.com>
2024-05-18 02:54:13 +0100

    solving deployment issueCannot find module @web/application Require stack: - /var/task/src/web/app.js Did you forget to add it to "dependencies" in `package.json`? Node.js process exited with exit status: 1. The logs above can help with debugging the issue. INIT_REPORT Init Duration: 112.55 ms	Phase: init	Status: error	Error Type: Runtime.ExitError Cannot find module @web/application Require stack: - /var/task/src/web/app.js Did you forget to add it to "dependencies" in `package.json`? Node.js process exited with exit status: 1. The logs above can help with debugging the issue. INIT_REPORT Init Duration: 195.36 ms	Phase: invoke	Status: error	Error Type: Runtime.ExitError

--- build/src/web/lib/base-middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseMiddleware = void 0;
-class BaseMiddleware {
-    constructor() {
-        this.execute = this.execute.bind(this);
-    }
-}
-exports.BaseMiddleware = BaseMiddleware;
--- src/web/app.ts
+++ src/web/app.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 
 
-import { App } from "@web/application";
-import "@core/config/redis.config";
-import "@core/config/redis.config";
+import { App } from "../web/application";
+import "../core/config/redis.config";
+import "../core/config/redis.config";
 
 export const bootstrap = async () => {
   new App().setup();
